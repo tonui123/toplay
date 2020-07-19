5861e038f4663c58c8e5048f80faf6fb8f864637
Skulljoker <skull27252@outlook.co.th>
2020-07-19 10:00:59 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> ยินดีต้อนรับ </h1>
+      <p>สิทธิกร เพชรฤทธิ์</p>
+    </div>
+  );
+}
+
+export default App;
